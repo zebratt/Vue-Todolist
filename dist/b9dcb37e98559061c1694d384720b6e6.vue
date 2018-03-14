aa23d961bfae2e1431c699f1a143e3368053e794
xuxuejian <jearbilove@yeah.net>
2018-03-14 17:55:15 +0800

    init repo

---
+++ dist/b9dcb37e98559061c1694d384720b6e6.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+      name: {{name}}
+  </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            name: 'callie'
+        }
+    }
+}
+</script>
+
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="App">
+        <div class="header">
+            <button v-on:click="addNewItem">Add new item</button>
+        </div>
+        <div class="items" :key="index" v-for="(item, index) in items">
+            {{item}}
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'app',
+    data: () => ({
+        items: [] 
+    }),
+    methods: {
+        addNewItem: () => {
+            console.log(123);
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+@import './App.scss';
+</style>
